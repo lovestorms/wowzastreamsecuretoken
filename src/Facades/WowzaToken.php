2071386b4c4ee7ed35fe596704ef9c6acd8eda19
Duc Do <ducdo@Ducs-MacBook-Pro.local>
2019-05-15 09:58:44 +0700

    first commit

---
+++ src/Facades/WowzaToken.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Lovestorms\WowzaStreamToken\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class WowzaToken extends Facade
+{
+	
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return 'wowzatoken';
+	}
+}
---
+++ src/WowzaTokenServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lovestorms\WowzaStreamToken;
+
+use Illuminate\Support\ServiceProvider;
+
+class WowzaTokenServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['wowzatoken'] = $this->app->share(
+            function () {
+                return new WowzaToken();
+            }
+        );
+    }
+}
